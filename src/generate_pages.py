afc7473b9865946f3685a4bbdf5e29bf63994c02
TmHive <rmush8713@gmail.com>
2026-03-29 19:07:56 +0300

    123

--- src/generate_pages.py
+++ src/generate_pages.py
@@ -1,8 +1,9 @@
 from markdown_to_html import *
 from extract_markdown import *
 import os
+import sys
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page {from_path} -> {dest_path}")
     with open(from_path, 'r') as f:
         md_content = f.read()
@@ -10,13 +11,13 @@ def generate_page(from_path, template_path, dest_path):
         template = f.read()
     html_string = markdown_to_html_node(md_content).to_html()
     title = extract_title(md_content)
-    html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string)
+    html = template.replace("{{ Title }}", title).replace("{{ Content }}", html_string).replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, 'w') as f:
         f.write(html)
 
 
-def recursive_generator(dir_path_content, template_path, dest_dir_path):
+def recursive_generator(dir_path_content, template_path, dest_dir_path, basepath='/'):
     if not os.path.exists(dest_dir_path):
         os.mkdir(dest_dir_path)
 
@@ -25,6 +26,6 @@ def recursive_generator(dir_path_content, template_path, dest_dir_path):
         to_ = os.path.join(dest_dir_path, n)
         to_index = os.path.join(dest_dir_path, 'index.html')
         if os.path.isfile(from_):
-            generate_page(from_, template_path, to_index)
+            generate_page(from_, template_path, to_index, basepath)
         else:
-            recursive_generator(from_, template_path, to_)
+            recursive_generator(from_, template_path, to_, basepath)
--- src/main.py
+++ src/main.py
@@ -3,15 +3,18 @@ import os
 import shutil
 from recursive_file_copy import *
 from generate_pages import *
+import sys
+
+basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
 from_dir = 'static'
-to_dir = 'public'
+to_dir = 'docs'
 
 def main():
     if os.path.exists(to_dir):
         shutil.rmtree(to_dir)
     copy_dir_tree(from_dir, to_dir)
-    recursive_generator('content', 'template.html', 'public')
+    recursive_generator('content', 'template.html', 'docs', basepath)
 
 
 main()
